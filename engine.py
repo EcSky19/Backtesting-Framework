d1b0cda2550c2df66ade0e94a66fdd4d135fbc93
Ethan Coskay <ethancoskay@gmail.com>
2025-01-22 16:28:59 -0500

    Added some implementation of the strategy

--- engine.py
+++ engine.py
@@ -115,8 +115,17 @@ class Trade():
 
 import yfinance as yf
 
-data = yf.Ticker('AAPL').history(start='2020-01-01', end='2022-12-31', interval='1d')
+class BuyAndSellSwitch(Strategy):
+    def on_bar(self):
+        if self.position_size == 0:
+            self.buy('AAPL', 1)
+            print(self.current_idx,"buy")
+        else:
+            self.sell('AAPL', 1)
+            print(self.current_idx,"sell")
+
+data = yf.Ticker('AAPL').history(start='2022-12-01', end='2022-12-31', interval='1d')
 e = Engine()
 e.add_data(data)
-e.add_strategy(Strategy())
-e.run()
+e.add_strategy(BuyAndSellSwitch())
+e.run()            
