16b8a3dee97a6611fc1b44d754f33503fa362f36
Ethan Coskay <ethancoskay@gmail.com>
2025-02-12 09:30:13 -0500

    Created a new class and can now run the program

--- engine.py
+++ engine.py
@@ -164,3 +164,21 @@ class Engine():
         total_return = 100 *((self.data.loc[self.current_idx]['Close'] * self.strategy.position_size + self.cash) / self.initial_cash -1)
         metrics['total_return'] = total_return
         return metrics#
+
+import yfinance as yf
+class BuyAndSellSwitch(Strategy):
+    def on_bar(self):
+        if self.position_size == 0:
+            limit_price = self.close * 0.995
+            self.buy_limit('AAPL', size=100,limit_price=limit_price)
+            print(self.current_idx,"buy")
+        else:
+            limit_price = self.close * 1.005
+            self.sell_limit('AAPL', size=100,limit_price=limit_price)
+            print(self.current_idx,"sell")
+            
+data = yf.Ticker('AAPL').history(start='2022-12-01', end='2022-12-31', interval='1d')
+e = Engine()
+e.add_data(data)
+e.add_strategy(BuyAndSellSwitch())
+e.run()   
